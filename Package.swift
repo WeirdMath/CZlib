d01e7eb4361e43441e8ed1ae030558d0e2b308dd
Sergej Jaskiewicz <seergy_pro@me.com>
2017-06-13 00:20:25 +0300

    Delete shim.h

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:3.1
+
 import PackageDescription
 
 let package = Package(
-    name: "CZlib"
+    name: "CZlib",
+    pkgConfig: "zlib",
+    providers: [
+        .Brew("zlib"),
+        .Apt("zlib1g-dev")
+    ]
 )
--- shim.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZLIB_HELPERS_H
-#define ZLIB_HELPERS_H
-
-#import <stdio.h>
-
-#import <zlib.h>
-
-
-#endif
